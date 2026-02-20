bd00e3b41ff45afecf458dd56863de5b53352cf7
bhuwan poudel <bkspoudel@gmail.com>
2026-02-20 11:41:20 +0100

    make a different function for yellow

--- src/game/feedback.js
+++ src/game/feedback.js
@@ -3,11 +3,22 @@ import chalk from "chalk";
 export function giveFeedback(guessWord, selectedWord) {
         let result = "";
         for(let i=0; i<guessWord.length; i++){
-                result += selectedWord[i] === guessWord[i]? chalk.green(guessWord[i]): selectedWord.includes(guessWord[i])? chalk.yellow(guessWord[i]) : chalk.gray(guessWord[i]);    
+                result += selectedWord[i] === guessWord[i]? chalk.green(guessWord[i]): 
+                selectedWord.includes(guessWord[i])? isItYellow(guessWord, i, selectedWord) : 
+                chalk.gray(guessWord[i]);    
         }
         return result;
     }
 
 export function errorMessage(){
         console.log(chalk.red("your guess is not valid. Try again."));
+}
+
+
+function isItYellow(guessWord, i, selectedWord) {
+
+        let count = selectedWord.split(guessWord[i]).length - 1;
+
+        return chalk.yellow(guessWord[i]);
+
 }
