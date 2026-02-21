a2d3ae33bb4d8034e4e0c151a91c9a4a9cf356a1
bhuwan poudel <bkspoudel@gmail.com>
2026-02-21 12:46:55 +0100

    save the changes as comment

--- src/game/feedback.js
+++ src/game/feedback.js
@@ -2,13 +2,12 @@ import chalk from "chalk";
 
 export function giveFeedback(guessWord, selectedWord) {
         let result = "";
-        let yellowGuess = {myGuess: ""}
         let wordInfo = getWordInfo(guessWord, selectedWord);
         console.log(wordInfo);
 
         for(let i=0; i<guessWord.length; i++){
                 result += selectedWord[i] === guessWord[i]? chalk.green(guessWord[i]): 
-                selectedWord.includes(guessWord[i])? isItYellow(guessWord, i, selectedWord, wordInfo, yellowGuess) : 
+                selectedWord.includes(guessWord[i])? isItYellow(guessWord, i, selectedWord, wordInfo) : 
                 chalk.gray(guessWord[i]);    
         }
         return result;
@@ -20,12 +19,11 @@ export function errorMessage(){
 
 
 
-function isItYellow(guessWord, i, selectedWord, wordInfo, yellowGuess) {
-        yellowGuess['myGuess'] = yellowGuess['myGuess'] + "" + guessWord[i]
+function isItYellow(guessWord, i, selectedWord, wordInfo) {
         let countInResult = selectedWord.split(guessWord[i]).length - 1;
-        let countInGuess =  yellowGuess['myGuess'].substring(0, i).split(guessWord[i]).length -2 ;
+        let countInGuess =  guessWord.substring(0, i).split(guessWord[i]).length -1 ;
         let countInExactPosition = wordInfo[guessWord[i]] ?? 0;
-        console.log(guessWord[i], yellowGuess['myGuess'], countInExactPosition, countInResult, countInGuess);
+        console.log(guessWord[i], countInExactPosition, countInResult, countInGuess);
         if(countInResult > (countInGuess + countInExactPosition)) {
                 return chalk.yellow(guessWord[i]);
                
@@ -60,6 +58,20 @@ function isItYellow(guessWord, i, selectedWord) {
         console.log(i, guessWord[i], guessWord.substring(0, i), alreadyWritten, inSelectedWord, count);
 
         if(count > (alreadyWritten + inSelectedWord.length -1) ) {
+                return chalk.yellow(guessWord[i]);
+               
+        }else {  
+                return chalk.gray(guessWord[i]);
+        }
+}
+
+function isItYellow(guessWord, i, selectedWord, wordInfo, yellowGuess) {
+        yellowGuess['myGuess'] = yellowGuess['myGuess'] + "" + guessWord[i]
+        let countInResult = selectedWord.split(guessWord[i]).length - 1;
+        let countInGuess =  yellowGuess['myGuess'].substring(0, i).split(guessWord[i]).length -2 ;
+        let countInExactPosition = wordInfo[guessWord[i]] ?? 0;
+        console.log(guessWord[i], yellowGuess['myGuess'], countInExactPosition, countInResult, countInGuess);
+        if(countInResult > (countInGuess + countInExactPosition)) {
                 return chalk.yellow(guessWord[i]);
                
         }else {  
