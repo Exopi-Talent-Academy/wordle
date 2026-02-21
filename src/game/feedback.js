7e053a2964ab171f9938bce0f2fa6bb976af2ef8
bhuwan poudel <bkspoudel@gmail.com>
2026-02-21 10:20:59 +0100

    add logic for number of yellow to display

--- src/game/feedback.js
+++ src/game/feedback.js
@@ -2,9 +2,12 @@ import chalk from "chalk";
 
 export function giveFeedback(guessWord, selectedWord) {
         let result = "";
+        let wordInfo = getWordInfo(guessWord, selectedWord);
+        // console.log(wordInfo);
+
         for(let i=0; i<guessWord.length; i++){
                 result += selectedWord[i] === guessWord[i]? chalk.green(guessWord[i]): 
-                selectedWord.includes(guessWord[i])? isItYellow(guessWord, i, selectedWord) : 
+                selectedWord.includes(guessWord[i])? isItYellow(guessWord, i, selectedWord, wordInfo) : 
                 chalk.gray(guessWord[i]);    
         }
         return result;
@@ -15,10 +18,51 @@ export function errorMessage(){
 }
 
 
-function isItYellow(guessWord, i, selectedWord) {
 
-        let count = selectedWord.split(guessWord[i]).length - 1;
-
-        return chalk.yellow(guessWord[i]);
-
+function isItYellow(guessWord, i, selectedWord, wordInfo) {
+        let countInResult = selectedWord.split(guessWord[i]).length - 1;
+        let countInGuess =  guessWord.substring(0, i).split(guessWord[i]).length -1 ;
+        let countInExactPosition = wordInfo[guessWord[i]] ?? 0;
+        // console.log(countInExactPosition, countInResult, countInGuess);
+        if(countInResult > (countInGuess + countInExactPosition)) {
+                return chalk.yellow(guessWord[i]);
+               
+        }else {  
+                return chalk.gray(guessWord[i]);
+        }
 }
+
+function getWordInfo( guessWord, selectedWord) {
+        let wordInfo = { };
+         for(let i=0; i<guessWord.length; i++){
+                if(selectedWord[i] === guessWord[i]) {
+                        if(wordInfo[guessWord[i]]) {
+                                wordInfo[guessWord[i]] += 1;
+                        }else {
+                                wordInfo[guessWord[i]] = 1;
+                        }
+                }  
+        }
+        return wordInfo;
+}
+
+
+
+/*
+
+function isItYellow(guessWord, i, selectedWord) {
+        let count = selectedWord.split(guessWord[i]).length - 1;
+        let cnt = guessWord.split(guessWord[i]).length - 1;
+        let alreadyWritten = guessWord.substring(0, i).split(guessWord[i]).length -1 ;
+        let inSelectedWord =  selectedWord.substring(i+1).split(guessWord[i]);
+        console.log(i, guessWord[i], guessWord.substring(0, i), alreadyWritten, inSelectedWord, count);
+
+        if(count > (alreadyWritten + inSelectedWord.length -1) ) {
+                return chalk.yellow(guessWord[i]);
+               
+        }else {  
+                return chalk.gray(guessWord[i]);
+        }
+}
+
+*/
