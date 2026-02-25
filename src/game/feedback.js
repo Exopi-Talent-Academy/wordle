8cc2506a254784d337f6532fdd3d9a05f3226326
bhuwan poudel <bkspoudel@gmail.com>
2026-02-25 19:18:50 +0100

    implement better logic to find the proper colors

--- src/game/feedback.js
+++ src/game/feedback.js
@@ -2,12 +2,59 @@ import chalk from "chalk";
 
 export function giveFeedback(guessWord, selectedWord) {
         let result = "";
+        let matchedResult = findProperColor(guessWord, selectedWord) ;
+        console.log(matchedResult);
         for(let i=0; i<guessWord.length; i++){
-                result += selectedWord[i] === guessWord[i]? chalk.green(guessWord[i]): selectedWord.includes(guessWord[i])? chalk.yellow(guessWord[i]) : chalk.gray(guessWord[i]);    
+                let color = matchedResult[i];
+                if(color === 'green') {
+                        result += chalk.green(guessWord[i]);
+                } else if(color === 'yellow') {
+                        result += chalk.yellow(guessWord[i]);
+                }else {
+                        result += chalk.gray(guessWord[i]);
+                }    
         }
         return result;
     }
 
 export function errorMessage(){
         console.log(chalk.red("your guess is not valid. Try again."));
+}
+
+function findProperColor(guessWord, targetWord) {
+        let matchedResult = {
+                0: '',
+                1: '',
+                2: '',
+                3: '',
+                4: ''
+        }
+        let guessWordArray = guessWord.split('');
+        let targetWordArray = targetWord.split('');
+
+        guessWordArray.forEach((char, index) => {
+                if(char === targetWordArray[index]) {
+                        matchedResult[index] = "green";
+                        guessWordArray[index] = '';
+                        targetWordArray[index] = '';
+                }
+        })
+
+         guessWordArray.forEach((char, index) => {
+                if(targetWordArray.includes(char)) {
+                        if(!char){
+                                return;
+                        }
+                        matchedResult[index] = 'yellow';
+                        guessWordArray[index] = '';
+                        let indexInTarget = targetWordArray.indexOf(char);
+                        targetWordArray[indexInTarget] = '';
+                }
+                else {
+                        matchedResult[index] = 'gray';
+                }
+        })
+
+        return matchedResult;
+        l
 }
